7c09cd426ae234ee2c61b4d7dbce2e96e507a5ef
Tomas Maly <malytomas@ucpu.cz>
2019-12-01 21:29:11 +0100

    more tests for enumerate

--- sources/test-core/enumerate.cpp
+++ sources/test-core/enumerate.cpp
@@ -1,8 +1,9 @@
-#include <vector>
-
 #include "main.h"
 #include <cage-core/enumerate.h>
 #include <cage-core/pointerRangeHolder.h>
+
+#include <vector>
+#include <map>
 
 namespace
 {
@@ -16,9 +17,20 @@ namespace
 		return res;
 	}
 
-	holder<pointerRange<const string>> genHolder()
+	holder<pointerRange<const string>> genRange()
 	{
 		return pointerRangeHolder<const string>(genStrings());
+	}
+
+	std::vector<holder<string>> genHolders()
+	{
+		std::vector<holder<string>> res;
+		res.push_back(detail::systemArena().createHolder<string>("ar"));
+		res.push_back(detail::systemArena().createHolder<string>("ma"));
+		res.push_back({});
+		res.push_back(detail::systemArena().createHolder<string>("ge"));
+		res.push_back(detail::systemArena().createHolder<string>("don"));
+		return res;
 	}
 }
 
@@ -36,7 +48,7 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("direct iterators");
 		uint32 i = 0;
-		for (auto it : enumerate(names.begin(), names.end()))
+		for (const auto &it : enumerate(names.begin(), names.end()))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(*it == names[it.cnt]);
@@ -48,7 +60,7 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("iterators with offset");
 		uint32 i = 0;
-		for (auto it : enumerate(names.begin(), names.end(), 5))
+		for (const auto &it : enumerate(names.begin(), names.end(), 5))
 		{
 			CAGE_TEST(*it == names[i]);
 			CAGE_TEST(it.cnt == i++ + 5);
@@ -59,7 +71,7 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("basic vector");
 		uint32 i = 0;
-		for (auto it : enumerate(names))
+		for (const auto &it : enumerate(names))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(*it == names[it.cnt]);
@@ -71,7 +83,7 @@ void testEnumerate()
 		CAGE_TESTCASE("temporary range");
 		auto range = enumerate(genStrings());
 		uint32 i = 0;
-		for (auto it : range)
+		for (const auto &it : range)
 			CAGE_TEST(it.cnt == i++);
 		CAGE_TEST(i == genStrings().size());
 	}
@@ -79,7 +91,7 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("temporary vector");
 		uint32 i = 0;
-		for (auto it : enumerate(genStrings()))
+		for (const auto &it : enumerate(genStrings()))
 			CAGE_TEST(it.cnt == i++);
 		CAGE_TEST(i == genStrings().size());
 	}
@@ -88,7 +100,7 @@ void testEnumerate()
 		CAGE_TESTCASE("const vector");
 		const std::vector<string> cns(names);
 		uint32 i = 0;
-		for (auto it : enumerate(cns))
+		for (const auto &it : enumerate(cns))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(*it == names[it.cnt]);
@@ -100,7 +112,7 @@ void testEnumerate()
 		CAGE_TESTCASE("c array");
 		const char *arr[] = { "a", "bb", "ccc" };
 		uint32 i = 0;
-		for (auto it : enumerate(arr))
+		for (const auto &it : enumerate(arr))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(*it == arr[it.cnt]);
@@ -111,7 +123,7 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("pointer range");
 		uint32 i = 0;
-		for (auto it : enumerate(pointerRange<const string>(names)))
+		for (const auto &it : enumerate(pointerRange<const string>(names)))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(*it == names[it.cnt]);
@@ -122,11 +134,42 @@ void testEnumerate()
 	{
 		CAGE_TESTCASE("holder of pointer range");
 		uint32 i = 0;
-		for (auto it : enumerate(genHolder()))
+		for (const auto &it : enumerate(genRange()))
 		{
 			CAGE_TEST(it.cnt == i++);
 			CAGE_TEST(it->length() > 0);
 		}
 		CAGE_TEST(i == genStrings().size());
+	}
+
+	{
+		CAGE_TESTCASE("vector of holders");
+		uint32 i = 0;
+		for (const auto &it : enumerate(genHolders()))
+		{
+			// unfortunately, we cannot use * (dereference operator), because that would attempt to create a copy of the holder
+			CAGE_TEST((!!it->get()) == (i != 2)); // test that the element at index 2 is empty holder
+			CAGE_TEST(it.cnt == i++);
+		}
+		CAGE_TEST(i == genHolders().size());
+	}
+
+	{
+		CAGE_TESTCASE("map");
+		std::map<string, string> orig;
+		orig["a"] = "a";
+		orig["b"] = "b";
+		orig["hello"] = "world";
+		orig["duck"] = "quack";
+		std::map<string, string> test;
+		uint32 i = 0;
+		for (const auto &it : enumerate(orig))
+		{
+			CAGE_TEST(it.cnt == i++);
+			test[it->first] = it->second;
+		}
+		CAGE_TEST(i == orig.size());
+		CAGE_TEST(test == orig);
+		CAGE_TEST(orig.size() == 4);
 	}
 }
