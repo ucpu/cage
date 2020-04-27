5e39c597ed9bb4a06a5e7500a25413b156bda1ae
Tomas Maly <malytomas@ucpu.cz>
2020-04-28 01:19:44 +0200

    use stb as a submodule

---
+++ externals/stb/stb.c
@@ -0,0 +1,10 @@
+
+#define STB_DXT_IMPLEMENTATION
+#include "stb_dxt.h"
+
+#define STB_RECT_PACK_IMPLEMENTATION
+#include "stb_rect_pack.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
