a6226aa684df2d6438bc2a1edea3dd3c8e32855a
Tomas Maly <malytomas@ucpu.cz>
2021-06-16 19:07:41 +0200

    constexpr FlatSet

--- sources/include/cage-core/flatSet.h
+++ sources/include/cage-core/flatSet.h
@@ -19,32 +19,32 @@ namespace cage
 		using const_reverse_iterator = typename std::vector<Value>::const_reverse_iterator;
 		using size_type = typename std::vector<Value>::size_type;
 
-		FlatSet() = default;
-		FlatSet(const FlatSet &other) = default;
-		FlatSet(FlatSet &&other) = default;
+		constexpr FlatSet() = default;
+		constexpr FlatSet(const FlatSet &other) = default;
+		constexpr FlatSet(FlatSet &&other) = default;
 		
 		template<class InputIt>
-		FlatSet(InputIt first, InputIt last)
+		constexpr FlatSet(InputIt first, InputIt last)
 		{
 			insert(first, last);
 		}
 
-		FlatSet(std::initializer_list<Value> init)
+		constexpr FlatSet(std::initializer_list<Value> init)
 		{
 			insert(init.begin(), init.end());
 		}
 
-		FlatSet &operator = (const FlatSet &other) = default;
-		FlatSet &operator = (FlatSet &&other) = default;
+		constexpr FlatSet &operator = (const FlatSet &other) = default;
+		constexpr FlatSet &operator = (FlatSet &&other) = default;
 
-		FlatSet &operator = (std::initializer_list<Value> ilist)
+		constexpr FlatSet &operator = (std::initializer_list<Value> ilist)
 		{
 			clear();
 			insert(ilist.begin(), ilist.end());
 			return *this;
 		}
 
-		std::pair<const_iterator, bool> insert(const Value &value)
+		constexpr std::pair<const_iterator, bool> insert(const Value &value)
 		{
 			auto it = std::lower_bound<const_iterator, Value, Compare>(data_.begin(), data_.end(), value, Compare());
 			if (it != data_.end() && equals(*it, value))
@@ -52,7 +52,7 @@ namespace cage
 			return { data_.insert(it, value), true };
 		}
 
-		std::pair<const_iterator, bool> insert(Value &&value)
+		constexpr std::pair<const_iterator, bool> insert(Value &&value)
 		{
 			auto it = std::lower_bound<const_iterator, Value, Compare>(data_.begin(), data_.end(), value, Compare());
 			if (it != data_.end() && equals(*it, value))
@@ -61,28 +61,28 @@ namespace cage
 		}
 
 		template<class InputIt>
-		void insert(InputIt first, InputIt last)
+		constexpr void insert(InputIt first, InputIt last)
 		{
 			while (first != last)
 				insert(*first++);
 		}
 
-		void insert(std::initializer_list<Value> ilist)
+		constexpr void insert(std::initializer_list<Value> ilist)
 		{
 			insert(ilist.begin(), ilist.end());
 		}
 
-		const_iterator erase(const_iterator pos)
+		constexpr const_iterator erase(const_iterator pos)
 		{
 			return data_.erase(pos);
 		}
 
-		const_iterator erase(const_iterator first, const_iterator last)
+		constexpr const_iterator erase(const_iterator first, const_iterator last)
 		{
 			return data_.erase(first, last);
 		}
 
-		uintPtr erase(const Value &value)
+		constexpr uintPtr erase(const Value &value)
 		{
 			auto it = std::lower_bound<const_iterator, Value, Compare>(data_.begin(), data_.end(), value, Compare());
 			if (it != data_.end() && equals(*it, value))
@@ -93,17 +93,17 @@ namespace cage
 			return 0;
 		}
 
-		void clear()
+		constexpr void clear()
 		{
 			data_.clear();
 		}
 
-		void reserve(uintPtr s)
+		constexpr void reserve(uintPtr s)
 		{
 			data_.reserve(s);
 		}
 
-		const_iterator find(const Value &value) const
+		constexpr const_iterator find(const Value &value) const
 		{
 			auto it = std::lower_bound<const_iterator, Value, Compare>(data_.begin(), data_.end(), value, Compare());
 			if (it != data_.end() && equals(*it, value))
@@ -111,42 +111,42 @@ namespace cage
 			return data_.end();
 		}
 
-		uintPtr count(const Value &value) const
+		constexpr uintPtr count(const Value &value) const
 		{
 			return std::binary_search<const_iterator, Value, Compare>(data_.begin(), data_.end(), value, Compare());
 		}
 
-		uintPtr size() const noexcept
+		constexpr uintPtr size() const noexcept
 		{
 			return data_.size();
 		}
 
-		bool empty() const noexcept
+		constexpr bool empty() const noexcept
 		{
 			return data_.empty();
 		}
 
-		const Value *data() const noexcept
+		constexpr const Value *data() const noexcept
 		{
 			return data_.data();
 		}
 
-		const_iterator begin() const noexcept
+		constexpr const_iterator begin() const noexcept
 		{
 			return data_.begin();
 		}
 
-		const_iterator end() const noexcept
+		constexpr const_iterator end() const noexcept
 		{
 			return data_.end();
 		}
 
-		const_reverse_iterator rbegin() const noexcept
+		constexpr const_reverse_iterator rbegin() const noexcept
 		{
 			return data_.rbegin();
 		}
 
-		const_reverse_iterator rend() const noexcept
+		constexpr const_reverse_iterator rend() const noexcept
 		{
 			return data_.rend();
 		}
@@ -154,12 +154,12 @@ namespace cage
 	private:
 		std::vector<Value> data_;
 
-		static bool equals (const Value &a, const Value &b)
+		static constexpr bool equals (const Value &a, const Value &b)
 		{
 			return !Compare()(a, b) && !Compare()(b, a);
 		}
 
-		friend bool operator == (const FlatSet &a, const FlatSet &b)
+		friend constexpr bool operator == (const FlatSet &a, const FlatSet &b)
 		{
 			return a.data_ == b.data_;
 		}
