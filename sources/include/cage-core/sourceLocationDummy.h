c7d597dea93de3c54ecd5b7731b95d75a864efa7
Tomas Maly <malytomas@ucpu.cz>
2022-01-24 03:39:17 +0100

    fix missing include

--- sources/include/cage-core/sourceLocationDummy.h
+++ sources/include/cage-core/sourceLocationDummy.h
@@ -1,14 +1,16 @@
 #ifndef guard_source_location_dummy_dfrg4sd5g
 #define guard_source_location_dummy_dfrg4sd5g
 
+#include <cstdint>
+
 namespace std
 {
 	struct source_location
 	{
 		static constexpr source_location current() noexcept { return {}; }
 		constexpr source_location() noexcept {}
-		constexpr std::uint_least32_t line() const noexcept { return 0; }
-		constexpr std::uint_least32_t column() const noexcept { return 0; }
+		constexpr std::uint32_t line() const noexcept { return 0; }
+		constexpr std::uint32_t column() const noexcept { return 0; }
 		constexpr const char *file_name() const noexcept { return ""; }
 		constexpr const char *function_name() const noexcept { return ""; }
 	};
