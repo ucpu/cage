5717b23a107da27d3dda83a372684fb244858526
Tomas Maly <malytomas@ucpu.cz>
2022-06-16 10:47:07 +0200

    improve decal shader

--- data/cage/shader/engine/decal.glsl
+++ data/cage/shader/engine/decal.glsl
@@ -14,17 +14,13 @@ $include ../functions/reconstructPosition.glsl
 
 layout(binding = CAGE_SHADER_TEXTURE_DEPTH) uniform sampler2D texDepth;
 
-#ifndef AlphaClip
-layout(early_fragment_tests) in;
-#endif
-
 void main()
 {
-	updateNormal();
-	Material material = loadMaterial();
 	vec3 prevPos = reconstructPosition(texDepth, uniViewport.vpInv, uniViewport.viewport);
 	if (length(varPosition - prevPos) > 0.05)
 		discard;
+	updateNormal();
+	Material material = loadMaterial();
 #ifdef AlphaClip
 	if (material.opacity < 0.5)
 		discard;
