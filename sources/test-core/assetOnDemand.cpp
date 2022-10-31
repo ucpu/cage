2afcc3ef722c51a51d79719c4c2df14351e5f312
Tomas Maly <malytomas@ucpu.cz>
2022-10-31 12:01:06 +0100

    fix tests for AssetOnDemand

--- sources/test-core/assetOnDemand.cpp
+++ sources/test-core/assetOnDemand.cpp
@@ -1,14 +1,23 @@
 #include "main.h"
 
+#include <cage-core/files.h>
 #include <cage-core/assetContext.h>
 #include <cage-core/assetManager.h>
 #include <cage-core/assetOnDemand.h>
 
 namespace
 {
+	const String AssetsPath = pathJoin(pathWorkingDir(), "testdir/assetManager/assets");
+
+	void processAssetDummyLoad(AssetContext *context)
+	{
+		// nothing
+	}
+
 	AssetScheme genAssetSchemeVoid()
 	{
 		AssetScheme s;
+		s.load.bind<&processAssetDummyLoad>();
 		s.typeHash = detail::typeHash<void>();
 		return s;
 	}
@@ -18,7 +27,10 @@ void testAssetOnDemand()
 {
 	CAGE_TESTCASE("asset on demand");
 
-	Holder<AssetManager> man = newAssetManager({});
+	pathCreateDirectories(AssetsPath);
+	AssetManagerCreateConfig cfg;
+	cfg.assetsFolderName = AssetsPath;
+	Holder<AssetManager> man = newAssetManager(cfg);
 	man->defineScheme<0, void>(genAssetSchemeVoid());
 	Holder<AssetOnDemand> cache = newAssetOnDemand(+man);
 	cache->get<0, void>(13);
