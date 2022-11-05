57bb7949526bb43923f6fe3d377df8bd133d96cc
Tomas Maly <malytomas@ucpu.cz>
2022-11-05 16:18:28 +0100

    move shadowmap helpers to engine from simple

---
+++ sources/include/cage-engine/sceneShadowmapFitting.h
@@ -0,0 +1,22 @@
+#ifndef guard_shadowmapFitting_h_FDF074217A66481BBFE8DFD4680D9E01
+#define guard_shadowmapFitting_h_FDF074217A66481BBFE8DFD4680D9E01
+
+#include <cage-engine/core.h>
+
+namespace cage
+{
+	class AssetManager;
+	class Entity;
+
+	struct CAGE_ENGINE_API ShadowmapFittingConfig
+	{
+		AssetManager *assets = nullptr;
+		Entity *light = nullptr;
+		Entity *camera = nullptr; // optional
+		uint32 sceneMask = m;
+	};
+
+	CAGE_ENGINE_API void shadowmapFitting(const ShadowmapFittingConfig &config);
+}
+
+#endif // guard_shadowmapFitting_h_FDF074217A66481BBFE8DFD4680D9E01
---
+++ sources/libengine/sceneShadowmapFitting.cpp
@@ -0,0 +1,91 @@
+#include <cage-core/geometry.h>
+#include <cage-core/entities.h>
+#include <cage-core/entitiesVisitor.h>
+#include <cage-core/assetManager.h>
+#include <cage-core/meshImport.h>
+#include <cage-engine/model.h>
+#include <cage-engine/renderObject.h>
+#include <cage-engine/scene.h>
+#include <cage-engine/sceneShadowmapFitting.h>
+
+namespace cage
+{
+	namespace
+	{
+		bool isEntityDirectionalLightWithShadowmap(Entity *light)
+		{
+			if (!light->has<TransformComponent>() || !light->has<LightComponent>() || !light->has<ShadowmapComponent>())
+				return false;
+			return light->value<LightComponent>().lightType == LightTypeEnum::Directional;
+		}
+
+		struct Boxes
+		{
+			AssetManager *assets = nullptr;
+
+			Aabb model(uint32 name) const
+			{
+				Holder<Model> m = assets->tryGet<AssetSchemeIndexModel, Model>(name);
+				if (m && any(m->flags & MeshRenderFlags::ShadowCast))
+					return m->boundingBox();
+				return Aabb();
+			}
+
+			Aabb object(uint32 name) const
+			{
+				Holder<RenderObject> o = assets->tryGet<AssetSchemeIndexRenderObject, RenderObject>(name);
+				if (!o)
+					return Aabb();
+				Aabb res;
+				for (uint32 i = 0; i < o->lodsCount(); i++) // not sure which LOD is loaded, so we need to check all
+					for (uint32 it : o->models(i))
+						res += model(it);
+				return res;
+			}
+
+			Aabb asset(uint32 name) const
+			{
+				{
+					Holder<Model> m = assets->tryGet<AssetSchemeIndexModel, Model>(name);
+					if (m)
+						return m->boundingBox();
+				}
+				{
+					Holder<RenderObject> o = assets->tryGet<AssetSchemeIndexRenderObject, RenderObject>(name);
+					if (o)
+						return object(name);
+				}
+				return Aabb();
+			}
+		};
+	}
+
+	void shadowmapFitting(const ShadowmapFittingConfig &config)
+	{
+		CAGE_ASSERT(isEntityDirectionalLightWithShadowmap(config.light));
+
+		uint32 mask = config.sceneMask;
+		if (config.camera)
+			mask &= config.camera->value<CameraComponent>().sceneMask;
+
+		// todo use camera frustum for fitting the shadowmap
+
+		Boxes boxes;
+		boxes.assets = config.assets;
+		Aabb box;
+		entitiesVisitor([&](Entity *e, const TransformComponent &t, const RenderComponent &r) {
+			if (r.sceneMask & mask)
+				box += boxes.asset(r.object) * t;
+		}, config.light->manager(), false);
+
+		TransformComponent &t = config.light->value<TransformComponent>();
+		ShadowmapComponent &s = config.light->value<ShadowmapComponent>();
+		if (box.empty())
+			s.worldSize = Vec3(1);
+		else
+		{
+			t.position = box.center();
+			s.worldSize = Vec3(box.diagonal() * 0.5);
+		}
+	}
+}
