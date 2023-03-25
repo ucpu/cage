8e89913c4ed24ae6ed9d0ea381eb11f9ab7e8c48
Tomas Maly <malytomas@ucpu.cz>
2023-03-25 05:09:00 +0100

    optimize serialization

--- sources/include/cage-core/containerSerialization.h
+++ sources/include/cage-core/containerSerialization.h
@@ -12,35 +12,115 @@ namespace cage
 			c.size();
 			c.begin();
 			c.end();
+			typename C::value_type;
 		};
 
 		template<class C>
 		concept ReservableContainerConcept = ContainerConcept<C> && requires(C c) {
-			c.reserve();
+			c.reserve(42);
 		};
+
+		template<class C>
+		concept InsertableContainerConcept = ContainerConcept<C> && requires(C c) {
+			c.insert(c.end(), *c.begin());
+		};
+
+		template<class C>
+		concept IsCageString = requires(C c) {
+			String(c);
+			C(String());
+		};
+
+		template<class C>
+		concept MemcpyContainerConcept = ContainerConcept<C> && std::is_trivially_copyable_v<std::remove_cvref_t<typename C::value_type>> && (!IsCageString<typename C::value_type>) && requires(C c) {
+			c.data();
+		};
+
+		template<class C>
+		concept MemcpyWritableContainerConcept = MemcpyContainerConcept<C> && requires(C c) {
+			c.resize(42);
+		};
+
+		template<class C>
+		concept ContainerOfBoolConcept = ContainerConcept<C> && std::is_same_v<std::remove_cvref_t<typename C::value_type>, bool>;
 	}
 
-	Serializer &operator << (Serializer &s, const privat::ContainerConcept auto &c)
+	template<class Cont> requires(privat::ContainerConcept<Cont>)
+	Serializer &operator << (Serializer &s, const Cont &c)
 	{
 		s << numeric_cast<uint64>(c.size());
-		for (const auto &it : c)
-			s << it;
+		if constexpr (privat::ContainerOfBoolConcept<Cont>)
+		{
+			const uint32 bytes = c.size() / 8;
+			for (uint32 i = 0; i < bytes; i++)
+			{
+				uint8 b = 0;
+				for (uint32 j = 0; j < 8; j++)
+					b |= (1u << j) * (c[i * 8 + j]);
+				s << b;
+			}
+			const uint32 rem = c.size() % 8;
+			if (rem)
+			{
+				uint8 b = 0;
+				for (uint32 j = 0; j < rem; j++)
+					b |= (1u << j) * (c[bytes * 8 + j]);
+				s << b;
+			}
+		}
+		else if constexpr (privat::MemcpyContainerConcept<Cont>)
+		{
+			s.write(bufferCast(PointerRange<const typename Cont::value_type>(c)));
+		}
+		else
+		{
+			for (const auto &it : c)
+				s << it;
+		}
 		return s;
 	}
 
-	Deserializer &operator >> (Deserializer &s, privat::ContainerConcept auto &c)
+	template<class Cont> requires(privat::InsertableContainerConcept<Cont>)
+	Deserializer &operator >> (Deserializer &s, Cont &c)
 	{
 		CAGE_ASSERT(c.empty());
 		uint64 size = 0;
 		s >> size;
-		if constexpr (privat::ReservableContainerConcept<decltype(c)>)
+		if constexpr (privat::ReservableContainerConcept<Cont>)
 			c.reserve(numeric_cast<decltype(c.size())>(size));
-		for (uint32 i = 0; i < size; i++)
+		if constexpr (privat::ContainerOfBoolConcept<Cont>)
 		{
-			using Tmp = std::remove_cv_t<std::remove_reference_t<decltype(*c.begin())>>;
-			Tmp tmp;
-			s >> tmp;
-			c.insert(c.end(), std::move(tmp));
+			const uint32 bytes = size / 8;
+			for (uint32 i = 0; i < bytes; i++)
+			{
+				uint8 b = 0;
+				s >> b;
+				for (uint32 j = 0; j < 8; j++)
+					c.insert(c.end(), !!(b & (1u << j)));
+			}
+			const uint32 rem = size % 8;
+			if (rem)
+			{
+				uint8 b = 0;
+				s >> b;
+				for (uint32 j = 0; j < rem; j++)
+					c.insert(c.end(), !!(b & (1u << j)));
+			}
+		}
+		else if constexpr (privat::MemcpyWritableContainerConcept<Cont>)
+		{
+			c.resize(numeric_cast<decltype(c.size())>(size));
+			s.read(bufferCast<char>(PointerRange<typename Cont::value_type>(c)));
+		}
+		else
+		{
+			for (uint32 i = 0; i < size; i++)
+			{
+				using Tmp = std::remove_cv_t<std::remove_reference_t<decltype(*c.begin())>>;
+				Tmp tmp;
+				s >> tmp;
+				c.insert(c.end(), std::move(tmp));
+			}
 		}
 		return s;
 	}
--- sources/test-core/containerSerialization.cpp
+++ sources/test-core/containerSerialization.cpp
@@ -10,16 +10,34 @@
 #include <unordered_map>
 #include <set>
 #include <unordered_set>
+#include <string>
 
 void testContainerSerialization()
 {
 	CAGE_TESTCASE("container serialization");
 
 	{
-		CAGE_TESTCASE("vector");
+		static_assert(privat::ContainerConcept<std::vector<uint32>>);
+		static_assert(privat::ReservableContainerConcept<std::vector<String>>);
+		static_assert(!privat::ReservableContainerConcept<std::set<String>>);
+		static_assert(privat::InsertableContainerConcept<std::vector<String>>);
+		static_assert(!privat::InsertableContainerConcept<const std::vector<String>>);
+		static_assert(privat::IsCageString<String>);
+		static_assert(privat::IsCageString<detail::StringBase<110>>);
+		static_assert(!privat::IsCageString<uint32>);
+		static_assert(privat::MemcpyContainerConcept<std::vector<uint32>>);
+		static_assert(!privat::MemcpyContainerConcept<std::list<uint32>>);
+		static_assert(!privat::MemcpyContainerConcept<std::vector<String>>);
+		static_assert(!privat::MemcpyContainerConcept<std::vector<detail::StringBase<110>>>);
+		static_assert(!privat::MemcpyContainerConcept<std::vector<std::string>>);
+		static_assert(privat::MemcpyWritableContainerConcept<std::vector<uint32>>);
+	}
+
+	{
+		CAGE_TESTCASE("vector of strings");
 		std::vector<String> cont;
 		cont.push_back("hello");
-		cont.push_back("world");
+		cont.push_back("darling");
 		MemoryBuffer buf;
 		Serializer ser(buf);
 		ser << cont;
@@ -28,15 +46,32 @@ void testContainerSerialization()
 		des >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont[0] == "hello");
-		CAGE_TEST(cont[1] == "world");
-		CAGE_TEST(buf.size() == 8 + 4 + 5 + 4 + 5); // make sure that the behavior is consistent for both 32 and 64 bit applications
+		CAGE_TEST(cont[1] == "darling");
+		CAGE_TEST(buf.size() == 8 + 4 + 5 + 4 + 7); // make sure that the behavior is consistent for both 32 and 64 bit applications
+	}
+
+	{
+		CAGE_TESTCASE("vector of uint32");
+		std::vector<uint32> cont;
+		cont.push_back(13);
+		cont.push_back(42);
+		MemoryBuffer buf;
+		Serializer ser(buf);
+		ser << cont;
+		cont.clear();
+		Deserializer des(buf);
+		des >> cont;
+		CAGE_TEST(cont.size() == 2);
+		CAGE_TEST(cont[0] == 13);
+		CAGE_TEST(cont[1] == 42);
+		CAGE_TEST(buf.size() == 8 + 4 + 4); // make sure that the behavior is consistent for both 32 and 64 bit applications
 	}
 
 	{
 		CAGE_TESTCASE("list");
 		std::list<String> cont;
 		cont.push_back("hello");
-		cont.push_back("world");
+		cont.push_back("darling");
 		MemoryBuffer buf;
 		Serializer ser(buf);
 		ser << cont;
@@ -45,7 +80,7 @@ void testContainerSerialization()
 		des >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont.front() == "hello");
-		CAGE_TEST(cont.back() == "world");
+		CAGE_TEST(cont.back() == "darling");
 	}
 
 	{
@@ -84,7 +119,7 @@ void testContainerSerialization()
 		CAGE_TESTCASE("set");
 		std::set<String> cont;
 		cont.insert("hello");
-		cont.insert("world");
+		cont.insert("darling");
 		MemoryBuffer buf;
 		Serializer ser(buf);
 		ser << cont;
@@ -93,14 +128,14 @@ void testContainerSerialization()
 		des >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont.count("hello") == 1);
-		CAGE_TEST(cont.count("world") == 1);
+		CAGE_TEST(cont.count("darling") == 1);
 	}
 
 	{
 		CAGE_TESTCASE("unordered_set");
 		std::unordered_set<String> cont;
 		cont.insert("hello");
-		cont.insert("world");
+		cont.insert("darling");
 		MemoryBuffer buf;
 		Serializer ser(buf);
 		ser << cont;
@@ -109,7 +144,7 @@ void testContainerSerialization()
 		des >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont.count("hello") == 1);
-		CAGE_TEST(cont.count("world") == 1);
+		CAGE_TEST(cont.count("darling") == 1);
 	}
 
 	{
@@ -165,7 +200,7 @@ void testContainerSerialization()
 		CAGE_TESTCASE("r-value deserializer for vector");
 		std::vector<String> cont;
 		cont.push_back("hello");
-		cont.push_back("world");
+		cont.push_back("darling");
 		MemoryBuffer buf;
 		Serializer ser(buf);
 		ser << cont;
@@ -174,14 +209,14 @@ void testContainerSerialization()
 		des.copy() >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont[0] == "hello");
-		CAGE_TEST(cont[1] == "world");
+		CAGE_TEST(cont[1] == "darling");
 	}
 
 	{
 		CAGE_TESTCASE("pointer range");
 		std::vector<String> cont;
 		cont.push_back("hello");
-		cont.push_back("world");
+		cont.push_back("darling");
 		PointerRange<const String> range = cont;
 		MemoryBuffer buf;
 		Serializer ser(buf);
@@ -191,8 +226,8 @@ void testContainerSerialization()
 		des >> cont;
 		CAGE_TEST(cont.size() == 2);
 		CAGE_TEST(cont[0] == "hello");
-		CAGE_TEST(cont[1] == "world");
-		CAGE_TEST(buf.size() == 8 + 4 + 5 + 4 + 5);
+		CAGE_TEST(cont[1] == "darling");
+		CAGE_TEST(buf.size() == 8 + 4 + 5 + 4 + 7);
 	}
 
 	{
@@ -211,5 +246,44 @@ void testContainerSerialization()
 		CAGE_TEST(cont.data()[0] == 13);
 		CAGE_TEST(cont.data()[1] == 20);
 		CAGE_TEST(cont.data()[2] == 42);
+	}
+
+	{
+		CAGE_TESTCASE("vector of bool");
+		std::vector<bool> cont;
+		cont.push_back(true);
+		cont.push_back(false);
+		cont.push_back(true);
+		cont.push_back(true);
+		cont.push_back(true);
+		MemoryBuffer buf;
+		Serializer ser(buf);
+		ser << cont;
+		cont.clear();
+		Deserializer des(buf);
+		des >> cont;
+		CAGE_TEST(cont.size() == 5);
+		CAGE_TEST(cont[0] == true);
+		CAGE_TEST(cont[1] == false);
+		CAGE_TEST(cont[2] == true);
+		CAGE_TEST(cont[3] == true);
+		CAGE_TEST(cont[4] == true);
+		CAGE_TEST(buf.size() == 8 + 1); // make sure that the three bits take one byte only
+	}
+
+	{
+		CAGE_TESTCASE("randomized vector of bool");
+		std::vector<bool> cont;
+		const uint32 size = rand() % 1000;
+		for (uint32 i = 0; i < size; i++)
+			cont.push_back((rand() % 2) == 0);
+		MemoryBuffer buf;
+		Serializer ser(buf);
+		ser << cont;
+		std::vector<bool> cont2;
+		Deserializer des(buf);
+		des >> cont2;
+		CAGE_TEST(cont == cont2);
+		CAGE_TEST(buf.size() < size / 8 + 10);
 	}
 }
