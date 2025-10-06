beabf6a2cffc336abe7afd2225915af83c7e7e1f
Tomáš Malý <malytomas@users.noreply.github.com>
2025-10-07 00:19:45 +0200

    cmake automatically find all external targets

---
+++ cmake/cage_list_all_targets.cmake
@@ -0,0 +1,10 @@
+
+function(cage_list_all_targets out_var current_dir)
+	get_property(targets DIRECTORY ${current_dir} PROPERTY BUILDSYSTEM_TARGETS)
+	get_property(subdirs DIRECTORY ${current_dir} PROPERTY SUBDIRECTORIES)
+	foreach(subdir ${subdirs})
+		cage_list_all_targets(subdir_targets ${subdir})
+		list(APPEND targets ${subdir_targets})
+	endforeach(subdir)
+	set(${out_var} ${targets} PARENT_SCOPE)
+endfunction()
