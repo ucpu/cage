23f75395928edd4a3ed7ef22c25e0bcb1a992761
Tomáš Malý <malytomas@users.noreply.github.com>
2025-10-08 04:05:55 +0200

    update GameNetworkingSockets; improve submodules script

--- submodules.sh
+++ submodules.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-git submodule update --init
-(cd externals/dawn/dawn && git submodule update --init "third_party/abseil-cpp" "third_party/protobuf" "third_party/spirv-headers/src" "third_party/spirv-tools/src" "third_party/vulkan-headers/src" "third_party/vulkan-utility-libraries/src" "third_party/jinja2" "third_party/markupsafe")
-(cd externals/wamr/zydis/dependencies && git submodule update --init)
+set -e
+
+update_submodule()
+{
+	local repo_path="$1"
+	local sub_path="$2"
+	pushd "$repo_path" >/dev/null
+	if [ -d "$sub_path/.git" ] || [ -f "$sub_path/.git" ]; then
+		echo "Skipping $repo_path/$sub_path"
+	else
+		echo "Initializing $repo_path/$sub_path"
+		git submodule update --init "$sub_path"
+	fi
+	popd >/dev/null
+}
+
+git submodule init
+
+git config --file .gitmodules --get-regexp path | awk '{print $2}' | while read -r sub; do
+	update_submodule "." "$sub"
+done
+
+update_submodule "externals/wamr/zydis" "dependencies/zycore"
+update_submodule "externals/dawn/dawn" "third_party/abseil-cpp"
+update_submodule "externals/dawn/dawn" "third_party/protobuf"
+update_submodule "externals/dawn/dawn" "third_party/spirv-headers/src"
+update_submodule "externals/dawn/dawn" "third_party/spirv-tools/src"
+update_submodule "externals/dawn/dawn" "third_party/vulkan-headers/src"
+update_submodule "externals/dawn/dawn" "third_party/vulkan-utility-libraries/src"
+update_submodule "externals/dawn/dawn" "third_party/jinja2"
+update_submodule "externals/dawn/dawn" "third_party/markupsafe"
